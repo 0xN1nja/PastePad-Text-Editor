769f4e5ea06c11e5e3ae9241c56bf0dee6c7ab70
Abhimanyu Sharma <71812498+0xN1nja@users.noreply.github.com>
2023-04-10 22:02:04 +0530

    Release 0.0.14
    Changelog :
    
    1.) Removed Comments From pastepad.py
    2.) Fixed Major Bug; After Saving File, exit_func Was Still Getting Called On WM_DELETE_WINDOW; Fixed It By Introducing is_saved State
    3.) Formatted Code
    4.) Added .gitignore And README.md
    5.) Generated New Release

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
+"""
+File : setup.py
+Author : Abhimanyu Sharma
+GitHub : https://github.com/0xN1nja
+"""
 import cx_Freeze
 import sys
-import os 
+import os
+
 base = None
 if sys.platform == 'win32':
     base = "Win32GUI"
-os.environ['TCL_LIBRARY'] = r"C:\Users\speed\AppData\Local\Programs\Python\Python39\tcl\tcl8.6"
-os.environ['TK_LIBRARY'] = r"C:\Users\speed\AppData\Local\Programs\Python\Python39\tcl\tk8.6"
+os.environ['TCL_LIBRARY'] = r"C:\Users\speed\AppData\Local\Programs\Python\Python310\tcl\tcl8.6"
+os.environ['TK_LIBRARY'] = r"C:\Users\speed\AppData\Local\Programs\Python\Python310\tcl\tk8.6"
 executables = [cx_Freeze.Executable("pastepad.py", base=base, icon="icon.ico")]
 cx_Freeze.setup(
-    name = "PastePad Text Editor",
-    options = {"build_exe": {"packages":["tkinter","os","webbrowser","datetime"], "include_files":["icon.ico",'tcl86t.dll','tk86t.dll', 'icons2']}},
-    version = "0.0.12",
+    name="PastePad Text Editor",
+    options={"build_exe": {"packages": ["tkinter", "os", "webbrowser", "datetime"],
+                           "include_files": ["icon.ico", 'tcl86t.dll', 'tk86t.dll', 'icons2']}},
+    version="0.0.14",
     author="Abhimanyu Sharma",
-    description = "PastePad Text Editor",
-    executables = executables
+    description="PastePad Text Editor",
+    executables=executables
 )
